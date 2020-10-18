8125eb9ab2cc730ae304434eb59deb86359887d5
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-18 23:33:13 +0300

    feat:realize check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arrStr = str.split(""),
+        pairBrackets = Object.fromEntries(bracketsConfig);
+    let workArr = [];
+    
+    for (let i = 0; i < arrStr.length; i++) {
+        if (arrStr[i] === pairBrackets[workArr[workArr.length - 1]]) {
+            if (workArr.length == 0) {
+                return false;
+            } else {
+                workArr.pop();
+            };
+        } else if (pairBrackets[arrStr[i]] !== undefined) {
+            workArr.push(arrStr[i]);
+        } else {
+            return false;
+        };
+    };
+
+    return workArr.length === 0;
 }
